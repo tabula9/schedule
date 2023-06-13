d9615358c205bf2a3f106ecaf5adf7b257157959
tabula9 <fhmtt484@gmail.com>
2023-06-13 18:25:38 +0900

    カラム追加とshoe実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params.require(:user).permit(:title, :start_date, :end_date, :all_date))
+    @user = User.new(params.require(:user).permit(:title, :start_date, :end_date, :all_date, :memo))
     if @user.save
       flash[:notice] = "予定を追加しました"
       redirect_to :users
@@ -18,6 +18,7 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_12_111622) do
+ActiveRecord::Schema.define(version: 2023_06_13_090023) do
 
   create_table "users", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2023_06_12_111622) do
     t.boolean "all_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "memo"
   end
 
 end
